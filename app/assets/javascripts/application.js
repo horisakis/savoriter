cf3fdeb2d478681b7e8d8d7904f53762e45ad26e
horisaki <seiji.horisaki@gmail.com>
2018-06-19 21:00:07 +0900

    jquery/bootstrapをrequire

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
