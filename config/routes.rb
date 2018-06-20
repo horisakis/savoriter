99a0009e3ef3b40ff8acd4126673510461100d76
horisaki <seiji.horisaki@gmail.com>
2018-06-20 17:34:44 +0900

    コピペミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      omniauth_callbacks: "users/omniauth_callbacks"
-    }
-  end
+  devise_for :users, controllers: {
+    # sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 end
