3f1a58a4385aecce4e26ca6e6922997a814bdb53
horisaki <seiji.horisaki@gmail.com>
2018-06-20 11:43:13 +0900

    devise初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
 end
