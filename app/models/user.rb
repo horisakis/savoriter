c559a12c16032fbbbdbf0525de5b44378ad285b7
horisaki <seiji.horisaki@gmail.com>
2018-06-20 15:11:48 +0900

    confirmation対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable, omniauth_providers: %i[google twitter]
 
   has_many :auths, dependent: :destroy
   has_many :favorites, dependent: :destroy
