51798b176ce9d353d97644e4dfd1722875848a50
horisaki <seiji.horisaki@gmail.com>
2018-06-20 17:31:51 +0900

    privateに変更、トランザクション追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,32 +29,37 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   # protected
-  def common_callback
-    auth_info = request.env['omniauth.auth']
-    auth = Auth.find_by_omniauth(auth_info)
-
-    if auth.present?
-      # @user = User.find(@auth.user_id)
-      @user = auth.user
-    else
-      if user_signed_in?
-        @user = current_user
-      else
-        @user = User.new(email: "#{auth_info.uid}.#{auth_info.provider}@example.com",
-                         password: Devise.friendly_token[0, 20])
-        @user.skip_confirmation!
-        @user.save
-      end
-
-      auth = Auth.create_from_omniauth(@user.id, auth_info)
-    end
-
-    sign_in_and_redirect @user, event: :authentication
-    set_flash_message(:notice, :success, kind: auth.provider) if is_navigational_format?
-  end
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  private
+
+  def common_callback
+    auth_info = request.env['omniauth.auth']
+    auth = Auth.find_by_omniauth(auth_info)
+
+    ActiveRecord::Base.transaction do
+      if auth.present?
+        # @user = User.find(@auth.user_id)
+        @user = auth.user
+      else
+        if user_signed_in?
+          @user = current_user
+        else
+          @user = User.new(email: "#{auth_info.uid}.#{auth_info.provider}@example.com",
+                           password: Devise.friendly_token[0, 20])
+          @user.skip_confirmation!
+          @user.save
+        end
+
+        auth = Auth.create_from_omniauth(@user.id, auth_info)
+      end
+    end
+
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: auth.provider) if is_navigational_format?
+  end
 end
