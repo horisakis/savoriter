ed40f388e6c2c620e131e056eb85e2181e54cdb6
horisaki <seiji.horisaki@gmail.com>
2018-06-23 12:01:26 +0900

    画像表示機能追加

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,9 @@
+class ContentsController < ApplicationController
+  def index
+    @contents = Content.joins(:favorites).select('provider', 'url', 'oembed')
+                       .order('favorites.id desc').limit(100)
+
+    # TODO: URLが重複している場合に除去する
+
+  end
+end
