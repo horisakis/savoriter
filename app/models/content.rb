8053410d76c7231bca8652e0fe16a589827c258c
horisaki <seiji.horisaki@gmail.com>
2018-12-03 19:31:42 +0900

    指定したsource_id以降を取得するscopeを定義

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,5 @@
 class Content < ApplicationRecord
   has_many :favorites, dependent: :destroy
   scope :provider, ->(provider) { where(provider: provider) }
+  scope :newer_by_source_id, ->(source_id) { where(source_id: source_id..Float::INFINITY) }
 end
