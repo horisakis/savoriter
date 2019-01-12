fdb6163ce4f01190f1667a05fab71095d7aa35c5
horisaki <seiji.horisaki@gmail.com>
2019-01-12 19:36:58 +0900

    スキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620040621) do
+ActiveRecord::Schema.define(version: 20190112100838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180620040621) do
     t.string "provider", null: false
     t.bigint "source_id", null: false
     t.text "url", null: false
-    t.text "oembed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
